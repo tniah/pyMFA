a02ee01eef66d4b113563f3a275d36a78188b515
Kai Nguyen <hai.nt@teko.vn>
2021-03-17 16:04:16 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ## pyMFA
  A library to support Multi Factor Authentication (TOPT, Email Token, SMS Token, etc.)
+
+## Installation ##
+
+### PypI ###
+```sh
+pip install pymfa
+```
+
+### Manual ###
+1. Clone the repository `git clone git@github.com:tniah/pyMFA.git`
+2. Enter it `cd pymfa`
+3. Install it `python setup.py install`
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyMFA',
+    version='0.9.1',
+    author='TNiaH',
+    author_email='kainguyen1509@gmail.com',
+    description='A Python library to support MFA',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tniah/pyMFA',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.7'
+)
